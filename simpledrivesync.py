b4953f3ec275548a883bec6b03684c1351138466
blobb <89486466+blobb999@users.noreply.github.com>
2023-04-10 11:35:40 +0200

    Update simpledrivesync.py

--- simpledrivesync.py
+++ simpledrivesync.py
@@ -3,12 +3,29 @@ import shutil
 import threading
 import ctypes
 import hashlib
-from tkinter import filedialog, messagebox, Tk, Button, Label, Entry, StringVar, Text, Scrollbar, END, N, S, E, W, Frame
+import xxhash
+from tkinter import filedialog, messagebox, Tk, Button, Label, Entry, StringVar, Text, Scrollbar, END, N, S, E, W, Frame, Checkbutton, BooleanVar
 from tkinter import ttk
 from concurrent.futures import ThreadPoolExecutor
+from tkinter.ttk import Sizegrip
 
 stop_sync_flag = False
 deleted_count = 0
+sync_thread = None
+
+def start_sync_thread():
+    global sync_thread
+    if sync_thread is not None and sync_thread.is_alive():
+        messagebox.showerror("Error", "A synchronization process is already running.")
+        return
+
+    sync_thread = threading.Thread(target=start_sync)
+    sync_thread.start()
+
+def update_status_text(text):
+    status_text.insert(END, text)
+    status_text.see(END)
+    root.after_idle(status_text.update)
 
 def copy_file(src_file_path, dest_file_path, status_text):
     try:
@@ -41,32 +58,44 @@ def update_progress_bar(progress_bar):
         progress_bar.grid_forget()
         return
     progress_bar.step(10)
-    progress_bar.after(100, lambda: update_progress_bar(progress_bar))
+    root.after(100, lambda: update_progress_bar(progress_bar))
 
-def should_copy(src_file_path, dest_file_path):
+def hash_file(file_path):
+    chunk_size = 1024 * 1024  # 1 MB
+    file_hash = xxhash.xxh64()
+
+    with open(file_path, 'rb') as file:
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+            file_hash.update(chunk)
+
+    return file_hash.hexdigest()
+
+def should_copy(src_file_path, dest_file_path, use_hash):
     if not os.path.exists(dest_file_path):
         return True
+
+    if not use_hash:
+        return False
 
     if os.path.getsize(src_file_path) != os.path.getsize(dest_file_path):
         return True
 
-    chunk_size = 1024 * 1024 # 1 MB
-    with open(src_file_path, 'rb') as src_file, open(dest_file_path, 'rb') as dest_file:
-        while True:
-            src_chunk = src_file.read(chunk_size)
-            dest_chunk = dest_file.read(chunk_size)
-            if src_chunk != dest_chunk:
-                return True
-            if not src_chunk:
-                break
+    src_hash = hash_file(src_file_path)
+    dest_hash = hash_file(dest_file_path)
 
-    return False
+    return src_hash != dest_hash
 
 def is_system_directory(folder_path):
     return os.path.isdir(folder_path) and ctypes.windll.kernel32.GetFileAttributesW(folder_path) & 0x4 == 0x4
 
-def remove_excess_files_and_dirs(src, dest, status_text):
+def remove_excess_files_and_dirs(src, dest, status_text, remove_excess):
     global deleted_count
+
+    if not remove_excess:
+        return
 
     for dest_root, dest_dirs, dest_files in os.walk(dest, topdown=False):
         src_root = os.path.join(src, os.path.relpath(dest_root, dest))
@@ -75,7 +104,6 @@ def remove_excess_files_and_dirs(src, dest, status_text):
             src_file_path = os.path.join(src_root, dest_file)
             dest_file_path = os.path.join(dest_root, dest_file)
 
-            # Check if the file exists in the source directory before deleting it
             if not os.path.exists(src_file_path) and has_admin_permission(dest_file_path):
                 try:
                     os.remove(dest_file_path)
@@ -92,7 +120,6 @@ def remove_excess_files_and_dirs(src, dest, status_text):
             src_dir_path = os.path.join(src_root, dest_dir)
             dest_dir_path = os.path.join(dest_root, dest_dir)
 
-            # Check if the directory exists in the source directory before deleting it
             if not os.path.exists(src_dir_path) and not is_system_directory(dest_dir_path) and has_admin_permission(dest_dir_path):
                 try:
                     shutil.rmtree(dest_dir_path)
@@ -105,12 +132,11 @@ def remove_excess_files_and_dirs(src, dest, status_text):
                     status_text.see(END)
                     status_text.update()
 
-            # If the directory exists in the source directory, remove it from the dest_dirs list to avoid
-            # removing it later in the loop
             elif os.path.exists(src_dir_path) and dest_dir in dest_dirs:
                 dest_dirs.remove(dest_dir)
 
-def sync_directories(src, dest, status_text, progress_bar):
+
+def sync_directories(src, dest, status_text, progress_bar, use_hash, remove_excess):
     copied_count = 0
     global deleted_count
     deleted_count = 0
@@ -143,7 +169,7 @@ def sync_directories(src, dest, status_text, progress_bar):
                         break
                     src_file_path = os.path.join(src_root, src_file)
                     dest_file_path = os.path.join(dest_root, src_file)
-                    if should_copy(src_file_path, dest_file_path):
+                    if should_copy(src_file_path, dest_file_path, use_hash.get()):
                         copied_count += 1
                         executor.submit(copy_file, src_file_path, dest_file_path, status_text)
 
@@ -153,101 +179,96 @@ def sync_directories(src, dest, status_text, progress_bar):
                     status_text.update()
                     return copied_count, deleted_count
 
-        remove_excess_files_and_dirs(src, dest, status_text)
+        remove_excess_files_and_dirs(src, dest, status_text, remove_excess.get())
 
     except Exception as e:
-        messagebox.showerror("Error", f"An error occurred during synchronization: {e}")
-
-    status_text.insert(END, f"Synchronization completed. {copied_count} files/directories copied, {deleted_count} files/directories deleted.\n")
-    progress_bar.grid_forget()
-    status_text.see(END)
-    status_text.update()
+        status_text.insert(END, f"Error during synchronization: {e}\n")
+        status_text.see(END)
+        status_text.update()
+    finally:
+        progress_bar.stop()
+        progress_bar.grid_forget()
 
     return copied_count, deleted_count
 
-def start_sync(status_text, progress_bar):
-    global stop_sync_flag
-    stop_sync_flag = False
-
+def start_sync():
     src = src_folder_path.get()
     dest = dest_folder_path.get()
 
     if not src or not dest:
-        messagebox.showerror("Error", "Both source and destination directories must be selected.")
+        messagebox.showerror("Error", "Both source and destination directories must be specified.")
         return
 
-    status_text.delete('1.0', END)
+    global stop_sync_flag
+    stop_sync_flag = False
 
-    progress_bar.grid(row=4, column=2, padx=5, pady=5)
-    progress_bar.start()
+    status_text.delete(1.0, END)
+    status_text.insert(END, "Synchronization started...\n")
+    status_text.see(END)
+    status_text.update()
+
+    progress_bar.grid(column=1, row=4, sticky=(E, W))
+    root.after(100, lambda: update_progress_bar(progress_bar))
     update_progress_bar(progress_bar)
 
-    sync_thread = threading.Thread(target=lambda: sync_directories(src, dest, status_text, progress_bar))
-    sync_thread.start()
+    copied_count, deleted_count = sync_directories(src, dest, status_text, progress_bar, use_hash, remove_excess)
 
+    status_text.insert(END, f"\nSynchronization finished. {copied_count} items copied, {deleted_count} items removed.\n")
+    status_text.see(END)
 
 def stop_sync():
     global stop_sync_flag
     stop_sync_flag = True
 
-def main():
-    global src_folder_path, dest_folder_path
+root = Tk()
+root.title("Directory Synchronization")
 
-    root = Tk()
-    root.title("Drive Synchronization Tool")
-    root.geometry("800x600")
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(7, weight=1)
+mainframe = ttk.Frame(root, padding="12 12 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
 
-    src_folder_path = StringVar()
-    dest_folder_path = StringVar()
+src_folder_path = StringVar()
+dest_folder_path = StringVar()
+use_hash = BooleanVar(value=False)
+remove_excess = BooleanVar(value=True)
 
-    src_label = Label(root, text="Source Directory:")
-    src_label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+ttk.Label(mainframe, text="Source directory:").grid(column=0, row=0, sticky=W)
+src_folder_entry = ttk.Entry(mainframe, width=50, textvariable=src_folder_path)
+src_folder_entry.grid(column=1, row=0, sticky=(E, W))
+ttk.Button(mainframe, text="Browse", command=browse_src_directory).grid(column=2, row=0, sticky=W)
 
-    src_entry = Entry(root, textvariable=src_folder_path, width=50)
-    src_entry.grid(row=1, column=0, sticky=W, padx=5)
+ttk.Label(mainframe, text="Destination directory:").grid(column=0, row=1, sticky=W)
+dest_folder_entry = ttk.Entry(mainframe, width=50, textvariable=dest_folder_path)
+dest_folder_entry.grid(column=1, row=1, sticky=(E, W))
+ttk.Button(mainframe, text="Browse", command=browse_dest_directory).grid(column=2, row=1, sticky=W)
 
-    src_browse_button = Button(root, text="Browse Source", command=browse_src_directory)
-    src_browse_button.grid(row=1, column=1, sticky=W, padx=5)
+ttk.Checkbutton(mainframe, text="Use xxhash to compare files", variable=use_hash).grid(column=1, row=2, sticky=W)
+ttk.Checkbutton(mainframe, text="Remove excess files and directories at the destination", variable=remove_excess).grid(column=1, row=3, sticky=W)
 
-    dest_label = Label(root, text="Destination Directory:")
-    dest_label.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+start_button = ttk.Button(mainframe, text="Start", command=start_sync_thread)
+start_button.grid(column=0, row=4, sticky=W)
 
-    dest_entry = Entry(root, textvariable=dest_folder_path, width=50)
-    dest_entry.grid(row=3, column=0, sticky=W, padx=5)
+progress_bar = ttk.Progressbar(mainframe, mode='indeterminate')
+progress_bar.grid(column=1, row=4, sticky=(E, W))
+progress_bar.grid_remove()
 
-    dest_browse_button = Button(root, text="Browse Destination", command=browse_dest_directory)
-    dest_browse_button.grid(row=3, column=1, sticky=W, padx=5)
+stop_button = ttk.Button(mainframe, text="Stop", command=stop_sync)
+stop_button.grid(column=2, row=4, sticky=W)
 
-    sync_button = Button(root, text="Start Sync", command=lambda: start_sync(status_text, progress_bar))
-    sync_button.grid(row=4, column=0, pady=10)
+status_label = ttk.Label(mainframe, text="Status:")
+status_label.grid(column=0, row=5, sticky=W)
 
-    stop_button = Button(root, text="Stop Sync", command=stop_sync)
-    stop_button.grid(row=4, column=1, pady=10)
+status_text = Text(mainframe, wrap="word", width=50, height=10)
+status_text.grid(column=1, row=5, sticky=(N, S, E, W))
+status_scroll = ttk.Scrollbar(mainframe, orient="vertical", command=status_text.yview)
+status_scroll.grid(column=2, row=5, sticky=(N, S, W))
+status_text["yscrollcommand"] = status_scroll.set
 
-    progress_bar_frame = Frame(root)
-    progress_bar_frame.grid(row=5, column=0, columnspan=2)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe.columnconfigure(1, weight=1)
+mainframe.rowconfigure(5, weight=1)
 
-    progress_bar = ttk.Progressbar(progress_bar_frame, mode='indeterminate', length=200)
-    progress_bar.pack(pady=10)
-    progress_bar.pack_forget()
+sizegrip = Sizegrip(mainframe)
+sizegrip.grid(column=2, row=6, sticky=(S, E))
 
-    status_label = Label(root, text="Synchronization Status:")
-    status_label.grid(row=6, column=0, sticky=W, padx=5, pady=5)
-
-    status_text = Text(root, wrap='word')
-    status_text.grid(row=7, column=0, columnspan=2, rowspan=2, sticky=N + S + E + W, padx=5, pady=5)
-
-    scrollbar = Scrollbar(root, command=status_text.yview)
-    scrollbar.grid(row=7, column=2, rowspan=2, sticky=N + S)
-
-    status_text.config(yscrollcommand=scrollbar.set)
-
-    sizegrip = ttk.Sizegrip(root)
-    sizegrip.grid(row=9, column=2, sticky=E + S)
-
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+root.mainloop()
